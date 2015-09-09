3a937346e151c611eed98f1687062eba478a30b6
Jonathan Moss <jmoss@snowballone.com.au>
2015-09-10 09:33:49 +1000

    Smaller resolution, fixed aspect ratio

--- physical/camera.py
+++ physical/camera.py
@@ -6,7 +6,7 @@ import picamera
 
 def capture_image():
     data = io.BytesIO()
-    with picamera.PiCamera(resolution=(800, 600), framerate=10) as camera:
+    with picamera.PiCamera(resolution=(640, 480), framerate=10) as camera:
         time.sleep(1)  # Camera warm-up time
         camera.rotation = 270
         camera.annotate_text = 'Hi MelbDjango!'
