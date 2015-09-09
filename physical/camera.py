b869bd0f692c88828cbd8a06c5939ca8dc2f2100
Jonathan Moss <jmoss@snowballone.com.au>
2015-09-10 09:36:29 +1000

    Adding background colour to annotation text

--- physical/camera.py
+++ physical/camera.py
@@ -2,6 +2,7 @@
 import io
 import time
 import picamera
+from picamera.color import Color
 
 
 def capture_image():
@@ -9,6 +10,7 @@ def capture_image():
     with picamera.PiCamera(resolution=(640, 480), framerate=10) as camera:
         time.sleep(1)  # Camera warm-up time
         camera.rotation = 270
+        camera.annotate_background = Color('#000')
         camera.annotate_text = 'Hi MelbDjango!'
         camera.capture(data, 'jpeg')
     data.seek(0)
